c5b862aad9a255517a529fecb7266fa2c3aceea4
Volodymyr Onishchenko <onwebdev@gmail.com>
2019-02-20 15:34:33 +0200

    Task 1 - Complete

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,7 +1,22 @@
 // Add your scripts here
-console.log('true');
+let money = prompt("Ваш бюджет на месяц?", ''),
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-const money = '';
-const time = '';
+let appData = {
+  budget: money,
+  optionalExpenses: {},
+  expenses: {},
+  income: [],
+  timeData: time,
+  savings: false,
+};
 
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", '');
+let a2 = prompt("Во сколько обойдется?", '');
+let a3 = prompt("Введите обязательную статью расходов в этом месяце", '');
+let a4 = prompt("Во сколько обойдется?", '');
 
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
